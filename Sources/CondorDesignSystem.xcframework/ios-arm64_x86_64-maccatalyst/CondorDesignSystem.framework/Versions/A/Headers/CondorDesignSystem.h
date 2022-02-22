386d4d94fbeb27fa4a5beb08f29df3c43c26fc52
Mohammed Sami <mohammed.sami@4sale.tech>
2022-02-22 16:56:32 +0200

    Initial Commit

---
+++ Sources/CondorDesignSystem.xcframework/ios-arm64_x86_64-maccatalyst/CondorDesignSystem.framework/Versions/A/Headers/CondorDesignSystem.h
@@ -0,0 +1,18 @@
+//
+//  CondorDesignSystem.h
+//  CondorDesignSystem
+//
+//  Created by Mohammed Sami on 02/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CondorDesignSystem.
+FOUNDATION_EXPORT double CondorDesignSystemVersionNumber;
+
+//! Project version string for CondorDesignSystem.
+FOUNDATION_EXPORT const unsigned char CondorDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CondorDesignSystem/PublicHeader.h>
+
+
